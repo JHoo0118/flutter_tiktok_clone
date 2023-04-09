1c363b48c844008f694d6e870acf1efe00c30d56
JHoo0118 <kik3078@gmail.com>
2023-04-09 17:48:11 +0900

    CupertinoTabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
 
 void main() {
@@ -9,27 +8,11 @@ void main() {
 class TikTokApp extends StatelessWidget {
   const TikTokApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const CupertinoApp(
       title: 'TikTok Clone',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFFE9435A),
-        appBarTheme: const AppBarTheme(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-      ),
-      home: const MainNavigationScreen(),
+      home: MainNavigationScreen(),
     );
   }
 }
