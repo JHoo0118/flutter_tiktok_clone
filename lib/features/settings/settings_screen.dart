4fb0d7248a0b30b5e941072b4a31a5564011216b
JHoo0118 <kik3078@gmail.com>
2023-05-05 00:49:05 +0900

    CupertinoAlertDialog

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -26,40 +27,20 @@ class _SettingsScreenState extends State<SettingsScreen> {
         ),
         body: ListView(
           children: [
-            Switch(
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-            ),
-            CupertinoSwitch(
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-            ),
-            Switch.adaptive(
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-            ),
-            SwitchListTile(
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
-              title: const Text("Enable notifications"),
-              subtitle: const Text("Enable notifications"),
-            ),
             SwitchListTile.adaptive(
               value: _notifications,
               onChanged: _onNotificationsChanged,
               title: const Text("Enable notifications"),
-            ),
-            Checkbox(
-              value: _notifications,
-              onChanged: _onNotificationsChanged,
+              subtitle: const Text("They will be cute."),
             ),
             CheckboxListTile(
               activeColor: Colors.black,
               value: _notifications,
               onChanged: _onNotificationsChanged,
               title: const Text(
-                "Enable notifications",
+                "Marketing emails",
               ),
+              subtitle: const Text("We won't spam you."),
             ),
             ListTile(
               onTap: () async {
@@ -93,7 +74,57 @@ class _SettingsScreenState extends State<SettingsScreen> {
               },
               title: const Text("What is your birthday?"),
             ),
-            const AboutListTile()
+            ListTile(
+              title: const Text("Log out (iOS)"),
+              textColor: Colors.red,
+              onTap: () {
+                showCupertinoDialog(
+                    context: context,
+                    builder: (context) => CupertinoAlertDialog(
+                          title: const Text("Are you sure?"),
+                          content: const Text("Plx dont go"),
+                          actions: [
+                            CupertinoDialogAction(
+                              onPressed: () => Navigator.of(context).pop(),
+                              child: const Text("No"),
+                            ),
+                            CupertinoDialogAction(
+                              onPressed: () => Navigator.of(context).pop(),
+                              isDestructiveAction: true,
+                              child: const Text("Yes"),
+                            ),
+                          ],
+                        ));
+              },
+            ),
+            ListTile(
+              title: const Text("Log out (Android)"),
+              textColor: Colors.red,
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    icon: const FaIcon(FontAwesomeIcons.skull),
+                    title: const Text("Are you sure?"),
+                    content: const Text("Plx dont go"),
+                    actions: [
+                      IconButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        icon: const FaIcon(FontAwesomeIcons.car),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("Yes"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            const AboutListTile(
+              applicationVersion: "1.0",
+              applicationLegalese: "Don't copy me.",
+            )
           ],
         ));
   }
