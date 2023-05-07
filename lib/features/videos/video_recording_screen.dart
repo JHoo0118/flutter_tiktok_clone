7f8d60a453807b414231416050f2abbdf15615fa
JHoo0118 <kik3078@gmail.com>
2023-05-07 20:07:13 +0900

    AppLifecycleState

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -15,9 +15,10 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen>
-    with TickerProviderStateMixin {
+    with TickerProviderStateMixin, WidgetsBindingObserver {
   bool _hasPermission = false;
   bool _isSelfieMode = false;
+  bool _prepareDispose = false;
   late FlashMode _flashMode;
 
   late CameraController _cameraController;
@@ -63,6 +64,8 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     await _cameraController.prepareForVideoRecording();
 
     _flashMode = _cameraController.value.flashMode;
+
+    setState(() {});
   }
 
   Future<void> initPermissions() async {
@@ -88,6 +91,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
   void initState() {
     super.initState();
     initPermissions();
+    WidgetsBinding.instance.addObserver(this);
     _progressAnimationController.addListener(() {
       setState(() {});
     });
@@ -144,6 +148,19 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     super.dispose();
   }
 
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (!_hasPermission || !_cameraController.value.isInitialized) return;
+    if (state == AppLifecycleState.inactive) {
+      _prepareDispose = true;
+      setState(() {});
+      _cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      _prepareDispose = false;
+      initCamera();
+    }
+  }
+
   Future<void> _onPickVideoPressed() async {
     final video = await ImagePicker().pickVideo(source: ImageSource.gallery);
     if (video == null) return;
@@ -179,7 +196,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
             : Stack(
                 alignment: Alignment.center,
                 children: [
-                  CameraPreview(_cameraController),
+                  if (!_prepareDispose) CameraPreview(_cameraController),
                   Positioned(
                     top: Sizes.size20,
                     right: Sizes.size20,
